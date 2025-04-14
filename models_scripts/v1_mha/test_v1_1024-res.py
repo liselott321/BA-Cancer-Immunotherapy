f26628738b1eca9863c8ee1abc8510ef01089467
liselott321 <arina.frohofer@gmail.com>
2025-04-14 10:14:59 +0000

    testlauf mit v19 best ht v1 res modell

--- models_scripts/v1_mha/test_v1_1024-res.py
+++ models_scripts/v1_mha/test_v1_1024-res.py
@@ -71,7 +71,7 @@ print("Lade Modell von wandb...")
 api = wandb.Api()
 runs = api.runs("ba_cancerimmunotherapy/dataset-allele")
 # Direktes Laden über bekannten Namen
-artifact_name = "ba_cancerimmunotherapy/dataset-allele/Run_v1_mha_1024h_model:v15" #anpassen, wenn andere version latest oder v12
+artifact_name = "ba_cancerimmunotherapy/dataset-allele/Run_v1_mha_1024h_model:v19" #anpassen, wenn andere version latest oder v12
 artifact = wandb.Api().artifact(artifact_name, type="model")
 artifact_dir = artifact.download()
 model_file = os.path.join(artifact_dir, os.listdir(artifact_dir)[0])
