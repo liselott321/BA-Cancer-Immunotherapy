bdf1801c950261a131788aea7367e3ea1034e0f6
liselott321 <arina.frohofer@gmail.com>
2025-05-29 08:53:22 +0000

    final runs v1 und v3

--- models_scripts/v1_mha/test_v1_1024-res.py
+++ models_scripts/v1_mha/test_v1_1024-res.py
@@ -78,6 +78,7 @@ api = wandb.Api()
 runs = api.runs("ba_cancerimmunotherapy/dataset-allele")
 # Direktes Laden über bekannten Namen
 artifact_name = "ba_cancerimmunotherapy/dataset-allele/Run_v1_mha_1024h_flattened_model:v36" #anpassen, wenn andere version latest oder v12
+#artifact_name = f"ba_cancerimmunotherapy/dataset-allele/model_checkpoint_epoch_{epoch_model}:v2"
 artifact = wandb.Api().artifact(artifact_name, type="model")
 artifact_dir = artifact.download()
 model_file = os.path.join(artifact_dir, os.listdir(artifact_dir)[0])
